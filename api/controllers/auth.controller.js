ddda8c0425a336a49e08fba0101b24f34daf8611
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-01-24 13:15:20 +0300

    created and tested signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -8,7 +11,7 @@ export const signup = async (req, res, next) => {
     try {
         //check if user exists
         const existingUser = await User.findOne({$or: [{email}, {username}]})
-        if(existingUser) return next(errorHandler(401,'Account with that email or username already exists'))
+        if(existingUser) return next(errorHandler(409,'Account with that email or username already exists'))
 
         //add new user to database
         const newUser = User({username, email, password: hashPassword});
@@ -18,6 +21,26 @@ export const signup = async (req, res, next) => {
         .json({message:'Account Created Successfully'})
     } catch (error) {
         next(error)
+    } 
+}
+
+export const signin = async (req, res, next) => {
+    try {
+        const {email, password} = req.body;
+        //checking if user details exist and authenticating
+        const authenticatedUser = await User.findOne({email});
+        if(!authenticatedUser) return next(errorHandler(404, 'Account does not exist'));
+        const authPassword = bcryptjs.compareSync(password,authenticatedUser.password);
+        if(!authPassword) return next(errorHandler(404, 'Wrong Credentials'));
+
+        //generating tokens for user if they are found to exist in the db
+        const token = jwt.sign({id: authenticatedUser._id}, process.env.JWT_KEY);
+        const {password: pass, ...rest} = authenticatedUser._doc
+        res
+        .cookie('access_token', token, {httpOnly: true})
+        .status(201)
+        .json(rest)
+    } catch (error) {
+       next(error) 
     }
-    
 }
