41336780f5d755f0dd14e2ed43ea5ef047dca255
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-01-24 14:32:42 +0300

    completed sign up functionality

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import { errorMiddleware } from './middlewares/error.middleware.js';
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to Database')
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,78 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id] : e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch ('http://localhost:3000/api/auth/signup', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success ===false) {
+        setLoading(false);
+        setError(data.message);
+        return
+      }
+      setLoading(false);
+      navigate('/signin')
+    } catch (error) {
+      setLoading(false);
+      setError(error.message)
+    }
+  }
   return (
    <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl font-bold text-center my-7'>Sign Up</h1>
-    <form className='flex flex-col p-3 gap-4'>
+    <form  onSubmit={handleSubmit} className='flex flex-col p-3 gap-4'>
       <input
         type='text'
         placeholder='username'
         id='username'
         className='border p-3 rounded-lg'
+        onChange = {handleChange}
       />
       <input
         type='email'
         placeholder='email'
         id='email'
         className='border p-3 rounded-lg'
+        onChange = {handleChange}
       />
       <input
         type='password'
         placeholder='password'
         id='password'
         className='border p-3 rounded-lg'
+        onChange = {handleChange}
       />
-      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
+      <button 
+      disabled ={loading}
+      className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+      >
+     {loading ? 'Loading...' : 'Sign Up'}
+      </button>
     </form>
-    <div className='flex gap-2 mt-5'>
+    <div className='flex gap-2 mt-5
+    onChange = {handleChange}'>
       <p>Already Have an Account?</p>
       <Link to ='/signin'>
         <span className='text-blue-700'>Sign In</span>
