40a5d089bf6f5c23d606f90f66510ff79baeca51
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-02-01 04:39:24 +0300

    home and about complete info

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold  mb-4 text-slate-800'>
+       Auth Web App!
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        This is a full-stack web application built with the MERN (MongoDB,
+        Express, React, Node.js) stack. It includes authentication features that
+        allow users to sign up, log in, and log out, and provides access to
+        protected routes only for authenticated users.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        The front-end of this web application is  based on React  and uses React
+        Router for client-side routing. The back-end of this web app is built with Node.js and
+        Express, and uses MongoDB atlas as the database. Authentication is implemented
+        using JSON Web Tokens (JWT).
+      </p>
     </div>
-  )
+  );
 }
