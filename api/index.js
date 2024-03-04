a4f1b1cf5b5da579377adc6f57fc0509988de973
SimiyuWafulah <simiyukelvinse@gmail.com>
2024-03-04 19:44:05 +0300

    changed cors origin url to  match the new url

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ import path from 'path'
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://auth-webapp.onrender.com',
     credentials: true
 }))
 app.use(cookieParser())
